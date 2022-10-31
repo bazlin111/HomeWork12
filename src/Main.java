435dfcc4ee431e50965bb67e09c4b47d446db569
bazlin111 <pbazlin@yandex.ru>
2022-10-31 11:20:01 +0400

    ДЗ Базлин П.Н,

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@
 public class Main {
     public static void main(String[] args) {
         Author chuckPalanic = new Author("Chuck", "Palanic");
-        Book fightClub = new Book("Fight Club", chuckPalanic, 1996);
+        Book fightClub = new Book("Fight Club", chuckPalanic, 1993);
         Author carlosCastaneda = new Author("Carlos", "Castaneda");
         Book fireFromWithin = new Book(" Fire From Within", carlosCastaneda, 1984);
 
 //        System.out.println(book.getName()+" "+book.getYearPublishing()+ " " +book.getAuthorName());
         System.out.println(" Книга 1 - " + fightClub.getName());
-        System.out.println(" Автор 1 - " + chuckPalanic.getFirstName()+ " " + chuckPalanic.getLastName());
+        System.out.println(" Автор 1 - " + chuckPalanic.getFirstName()+ " " + chuckPalanic.getLastName()+" "+ fightClub.getYear());
         System.out.println(" Книга 2 - " + fireFromWithin.getName());
-        System.out.println(" Автор 2 - " + carlosCastaneda.getFirstName()+ " "+ carlosCastaneda.getLastName());
+        System.out.println(" Автор 2 - " + carlosCastaneda.getFirstName()+ " "+ carlosCastaneda.getLastName() + " "+ fireFromWithin.getYear());
+        System.out.println(chuckPalanic);
+        System.out.println(carlosCastaneda);
+        System.out.println(fightClub);
+        System.out.println(fireFromWithin);
     }
 }
